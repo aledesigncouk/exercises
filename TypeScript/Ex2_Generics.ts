ea27d20f45614835012abb9eeaed9d5d7f21825d
aledesigncouk <alex.deidda@aledesign.co.uk>
2025-04-08 14:53:09 +0100

    error fix

--- TypeScript/Ex2_Generics.ts
+++ TypeScript/Ex2_Generics.ts
@@ -3,16 +3,18 @@
 Problem: Implement a generic stack class with push, pop, and peek methods. */
 
 class Stack<T> {
-    private elements: T[] = [];
-  
-    push(element: T): void {
-      // Implement this method
-    }
-  
-    pop(): T | undefined {
-      // Implement this method
-      return undefined;
-    }
-  
-    peek(): T | undefined {
-      // Implement this method
+  private elements: T[] = [];
+
+  push(element: T): void {
+    // Implement this method
+  }
+
+  pop(): T | undefined {
+    // Implement this method
+    return undefined;
+  }
+
+  peek(): T | undefined {
+    // Implement this method
+  }
+}
